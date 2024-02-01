e4d7432ab87b8a58ce5b329c63fac3d08ac9cc06
rick2785 <rjhrabowskie@gmail.com>
2024-02-01 00:45:18 -0600

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by RJ Hrabowskie on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
